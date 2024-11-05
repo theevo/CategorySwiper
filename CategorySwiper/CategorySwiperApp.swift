7bc20716040c8ed0fcb64648bbce22fd24786606
Theo Vora <theo.vora@gmail.com>
2024-11-05 15:15:27 -0600

    Initial Commit

---
+++ CategorySwiper/CategorySwiperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CategorySwiperApp.swift
+//  CategorySwiper
+//
+//  Created by Tana Vora on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CategorySwiperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CategorySwiper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CategorySwiper
+//
+//  Created by Tana Vora on 11/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
