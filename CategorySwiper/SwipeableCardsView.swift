3e6322091d9939dfe43e969b903b32f2e3419a8a
Theo Vora <theo.vora@gmail.com>
2024-12-06 17:16:14 -0600

    show NoTransactionsView if no transactions were loaded; show SwipedAllCardView after all cards are swiped

--- CategorySwiper/SwipeableCardsView.swift
+++ CategorySwiper/SwipeableCardsView.swift
@@ -14,45 +14,51 @@ struct SwipeableCardsView: View {
     private let swipeThreshold: CGFloat = 100.0
     
     var body: some View {
-        ZStack {
+        if model.isEmpty {
             NoTransactionsView()
-            ForEach($model.unswipedCards.reversed()) { card in
-                let isTop = model.isTopCard(card: card.wrappedValue)
-                let isSecond = model.isSecondCard(card: card.wrappedValue)
-                
-                CardView(
-                    transaction: card,
-                    isTop: isTop,
-                    isSecond: isSecond,
-                    dragOffset: dragState
-                )
-                .offset(x: isTop ? dragState.width : 0)
-                .gesture(
-                    DragGesture()
-                        .onChanged { gesture in
-                            self.dragState = gesture.translation
-                            //                    self.cardRotation = Double(gesture.translation.width) / rotationFactor
-                        }
-                        .onEnded { value in
-                            if abs(self.dragState.width) > swipeThreshold {
-                                let swipeDirection: CardViewModel.SwipeDirection = self.dragState.width > 0 ? .right : .left
-                                model.updateTopCardSwipeDirection(swipeDirection)
-                                
-                                withAnimation(.easeOut(duration: 0.5)) {
-                                    self.dragState.width = self.dragState.width > 0 ? 1000 : -1000
-                                }
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                    self.model.removeTopCard()
-                                    self.dragState = .zero
-                                }
-                            } else {
-                                withAnimation(.bouncy(extraBounce:0.21)) {
-                                    self.dragState = .zero
-                                    //                            self.cardRotation = 0
+        } else if model.isDoneSwiping {
+            SwipedAllCardsView()
+        }
+        else {
+            ZStack {
+                ForEach($model.unswipedCards.reversed()) { card in
+                    let isTop = model.isTopCard(card: card.wrappedValue)
+                    let isSecond = model.isSecondCard(card: card.wrappedValue)
+                    
+                    CardView(
+                        transaction: card,
+                        isTop: isTop,
+                        isSecond: isSecond,
+                        dragOffset: dragState
+                    )
+                    .offset(x: isTop ? dragState.width : 0)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { gesture in
+                                self.dragState = gesture.translation
+                                //                    self.cardRotation = Double(gesture.translation.width) / rotationFactor
+                            }
+                            .onEnded { value in
+                                if abs(self.dragState.width) > swipeThreshold {
+                                    let swipeDirection: CardViewModel.SwipeDirection = self.dragState.width > 0 ? .right : .left
+                                    model.updateTopCardSwipeDirection(swipeDirection)
+                                    
+                                    withAnimation(.easeOut(duration: 0.5)) {
+                                        self.dragState.width = self.dragState.width > 0 ? 1000 : -1000
+                                    }
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                        self.model.removeTopCard()
+                                        self.dragState = .zero
+                                    }
+                                } else {
+                                    withAnimation(.bouncy(extraBounce:0.21)) {
+                                        self.dragState = .zero
+                                        //                            self.cardRotation = 0
+                                    }
                                 }
                             }
-                        }
-                )
+                    )
+                }
             }
         }
     }
