f30e61da4f7930f45da2e4133f122c1c4fe941c1
Theo Vora <theo.vora@gmail.com>
2025-01-01 17:07:41 -0600

    init with optional Date

--- CategorySwiper/MonthRangeBuilder.swift
+++ CategorySwiper/MonthRangeBuilder.swift
@@ -9,18 +9,13 @@ import Foundation
 import Time
 
 struct MonthRangeBuilder {
-    init() {
-        let systemClock = Clocks.system
-        let thisMonth = systemClock.currentMonth
-        let previousMonth = systemClock.previousMonth
-        
-        let firstDay = previousMonth.firstDay
-        let lastDay = previousMonth.lastDay
-        
-        let firstDayFormatted = firstDay.format(year: .naturalDigits, month: .twoDigits, day: .twoDigits)
-        let lastDayFormatted = lastDay.format(year: .naturalDigits, month: .twoDigits, day: .twoDigits)
-        
-        print(firstDayFormatted)
-        print(lastDayFormatted)
+    var clock: any RegionalClock
+    
+    init(currentDate: Date? = nil) {
+        if let currentDate = currentDate {
+            self.clock = Clocks.custom(startingFrom: Instant(date: currentDate))
+        } else {
+            self.clock = Clocks.system
+        }
     }
 }
--- CategorySwiperTests/MonthRangeBuilderTests.swift
+++ CategorySwiperTests/MonthRangeBuilderTests.swift
@@ -9,8 +9,25 @@ import XCTest
 @testable import CategorySwiper
 
 class MonthRangeBuilderTests: XCTestCase {
+    func test_initWithDate_yieldsCorrectString() {
+        let currentDate = utcDate(from: "1980-06-05")
+        let sut = MonthRangeBuilder(currentDate: currentDate)
+        XCTAssertEqual(sut.clock.currentDay.format(date: .short), "6/5/80")
+    }
     
-    func testZero() throws {
-        let sut = MonthRangeBuilder()
+    // MARK: - Helpers
+    fileprivate func utcDate(from str: String) -> Date {
+        let formatter = DateFormatter.inUTCTimeZone()
+        guard let date = formatter.date(from: str) else { fatalError() }
+        return date
+    }
+}
+
+extension DateFormatter {
+    public static func inUTCTimeZone(dateFormat: String = "yyyy-MM-dd") -> DateFormatter {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = dateFormat
+        return formatter
     }
 }
